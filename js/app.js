576a2b05c9fb7cb0a424f7a576a73ce2fc956f8d
Burhan M <mirojburhanudin2@gmail.com>
2021-02-06 21:52:30 +0700

    Progdi page

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 import "./animation/swiper.js";
 import "./animation/animation.js";
-
 // component
 import "./components/footer.js";
--- js/components/galery.js
+++ js/components/galery.js
@@ -3,13 +3,13 @@ $(document).ready(function() {
     $('.ppdb-btn').magnificPopup({
         items: [
             {
-                src: 'https://i.ibb.co/NTQ565P/ppdb-slide1.jpg',
+                src: '/img/Muhamka-galeri/PPDB/ppdb-slide1.jpg',
             },
             {
-                src: 'https://i.ibb.co/pPNR6VB/ppdb-lide2.jpg'
+                src: '/img/Muhamka-galeri/PPDB/ppdb-slide2.jpg'
             },
             {
-                src: 'https://i.ibb.co/kBCq6t1/ppdb-slide3.jpg'
+                src: '/img/Muhamka-galeri/PPDB/ppdb-slide3.jpg'
             }
     ],
         gallery: {
@@ -21,25 +21,25 @@ $(document).ready(function() {
     $('.prestasi-btn').magnificPopup({
         items: [
             {
-                src: 'https://i.ibb.co/CwmjhxC/prestasi-slide1.jpg',
+                src: '/img/Muhamka-galeri/Prestasi/prestasi-slide1.jpg',
             },
             {
-                src: 'https://i.ibb.co/JRLPNK5/prestasi-slide2.jpg',
+                src: '/img/Muhamka-galeri/Prestasi/prestasi-slide2.jpg',
             },
             {
-                src: 'https://i.ibb.co/GcCcv0D/prestasi-slide3.jpg',
+                src: '/img/Muhamka-galeri/Prestasi/prestasi-slide3.jpg',
             },
             {
-                src: 'https://i.ibb.co/Rj4Z7X7/prestasi-slide4.jpg',
+                src: '/img/Muhamka-galeri/Prestasi/prestasi-slide4.jpg',
             },
             {
-                src: 'https://i.ibb.co/zsQKV69/prestasi-slide5.jpg',
+                src: '/img/Muhamka-galeri/Prestasi/prestasi-slide5.jpg',
             },
             {
-                src: 'https://i.ibb.co/DWQknwT/prestasi-slide6.jpg',
+                src: '/img/Muhamka-galeri/Prestasi/prestasi-slide6.jpg',
             },
             {
-                src: ' https://i.ibb.co/DksdFCh/prestasi-slide7.jpg',
+                src: '/img/Muhamka-galeri/Prestasi/prestasi-slide7.jpg',
             },
     ],
         gallery: {
@@ -52,22 +52,22 @@ $(document).ready(function() {
     $('.kunjungan-mendikbud-btn').magnificPopup({
         items: [
             {
-                src: 'https://i.ibb.co/vzxTbDy/kunjungan-mendikbud-slide1.jpg',
+                src: '/img/Muhamka-galeri/Kunjungan-mendikbud/kunjungan-mendikbud-slide1.jpg',
             },
             {
-                src: 'https://i.ibb.co/vjCjDrb/kunjungan-mendikbud-slide2.jpg',
+                src: '/img/Muhamka-galeri/Kunjungan-mendikbud/kunjungan-mendikbud-slide2.jpg',
             },
             {
-                src: 'https://i.ibb.co/L8sHWd6/kunjungan-mendikbud-slide3.jpg',
+                src: '/img/Muhamka-galeri/Kunjungan-mendikbud/kunjungan-mendikbud-slide3.jpg',
             },
             {
-                src: 'https://i.ibb.co/wz3Q7f5/kunjungan-mendikbud-slide4.jpg',
+                src: '/img/Muhamka-galeri/Kunjungan-mendikbud/kunjungan-mendikbud-slide4.jpg',
             },
             {
-                src: 'https://i.ibb.co/wWppJqT/kunjungan-mendikbud-slide5.jpg',
+                src: '/img/Muhamka-galeri/Kunjungan-mendikbud/kunjungan-mendikbud-slide5.jpg',
             },
             {
-                src: 'https://i.ibb.co/jWvPYWM/kunjungan-mendikbud-slide6.jpg',
+                src: '/img/Muhamka-galeri/Kunjungan-mendikbud/kunjungan-mendikbud-slide6.jpg',
             },
     ],
         gallery: {
@@ -79,10 +79,10 @@ $(document).ready(function() {
     $('.kampus-btn').magnificPopup({
         items: [
             {
-                src: 'https://i.ibb.co/HxB41gJ/gedung-Utama-slide1.jpg',
+                src: '/img/Muhamka-galeri/Kampus/gedungUtama-slide1.jpg',
             },
             {
-                src: 'https://i.ibb.co/VS6xQVW/gedung-F-slide2.jpg',
+                src: '/img/Muhamka-galeri/Kampus/gedungF-slide2.jpg',
             },
     ],
         gallery: {
@@ -94,28 +94,28 @@ $(document).ready(function() {
     $('.praktikum-btn').magnificPopup({
         items: [
             {
-                src: 'https://i.ibb.co/Lr8Rpjm/praktek-tkr-slide1.jpg',
+                src: '/img/Muhamka-galeri/Praktek/praktek-tkr-slide1.jpg',
             },
             {
-                src: 'https://i.ibb.co/7zDgqDx/praktek-ka-slide2.jpg',
+                src: '/img/Muhamka-galeri/Praktek/praktek-ka-slide2.jpg',
             },
             {
-                src: 'https://i.ibb.co/3BMYd31/praktek-tkr-slide3.jpg',
+                src: '/img/Muhamka-galeri/Praktek/praktek-tkr-slide3.jpg',
             },
             {
-                src: 'https://i.ibb.co/wJR6d6J/praktek-tkj-slide4.jpg',
+                src: '/img/Muhamka-galeri/Praktek/praktek-tkj-slide4.jpg',
             },
             {
-                src: 'https://i.ibb.co/SnQSWWm/praktek-ki-slide5.jpg',
+                src: '/img/Muhamka-galeri/Praktek/praktek-ki-slide5.jpg',
             },
             {
-                src: 'https://i.ibb.co/47fMz73/praktek-tkr-slide6.jpg',
+                src: '/img/Muhamka-galeri/Praktek/praktek-tkr-slide6.jpg',
             },
             {
-                src: 'https://i.ibb.co/qpChqRv/praktek-tkr-slide8.jpg',
+                src: '/img/Muhamka-galeri/Praktek/praktek-av-slide7.jpg',
             },
             {
-                src: 'https://i.ibb.co/xH1HPbG/praktek-av-slide7.jpg',
+                src: '/img/Muhamka-galeri/Praktek/praktek-tkr-slide8.jpg',
             },
     ],
         gallery: {
@@ -127,61 +127,61 @@ $(document).ready(function() {
     $('.kegiatan-siswa-btn').magnificPopup({
         items: [
             {
-                src: 'https://i.ibb.co/JvLMk0v/kegiatan-siswa-slide1.jpg',
+                src: '/img/Muhamka-galeri/kegiatan-siswa/kegiatan-siswa-slide1.jpg',
             },
             {
-                src: 'https://i.ibb.co/8NmF5TW/kegiatan-siswa-slide2.jpg',
+                src: '/img/Muhamka-galeri/kegiatan-siswa/kegiatan-siswa-slide2.jpg',
             },
             {
-                src: 'https://i.ibb.co/YNVvpmw/kegiatan-siswa-slide3.jpg',
+                src: '/img/Muhamka-galeri/kegiatan-siswa/kegiatan-siswa-slide3.jpg',
             },
             {
-                src: 'https://i.ibb.co/1bDxMJr/kegiatan-siswa-slide4.jpg',
+                src: '/img/Muhamka-galeri/kegiatan-siswa/kegiatan-siswa-slide4.jpg',
             },
             {
-                src: 'https://i.ibb.co/QX9LrgX/kegiatan-siswa-slide5.jpg',
+                src: '/img/Muhamka-galeri/kegiatan-siswa/kegiatan-siswa-slide5.jpg',
             },
             {
-                src: 'https://i.ibb.co/TBJBVQV/kegiatan-siswa-slide6.jpg',
+                src: '/img/Muhamka-galeri/kegiatan-siswa/kegiatan-siswa-slide6.jpg',
             },
             {
-                src: 'https://i.ibb.co/HpvHkZF/kegiatan-siswa-slide7.jpg',
+                src: '/img/Muhamka-galeri/kegiatan-siswa/kegiatan-siswa-slide7.jpg',
             },
             {
-                src: 'https://i.ibb.co/PZ2BfKs/kegiatan-siswa-slide8.jpg',
+                src: '/img/Muhamka-galeri/kegiatan-siswa/kegiatan-siswa-slide8.jpg',
             },
             {
-                src: 'https://i.ibb.co/1L8nprn/kegiatan-siswa-slide9.jpg',
+                src: '/img/Muhamka-galeri/kegiatan-siswa/kegiatan-siswa-slide9.jpg',
             },
             {
-                src: 'https://i.ibb.co/Q6Bk8P9/kegiatan-siswa-slide10.jpg',
+                src: '/img/Muhamka-galeri/kegiatan-siswa/kegiatan-siswa-slide10.jpg',
             },
             {
-                src: 'https://i.ibb.co/488GdJV/kegiatan-siswa-slide11.jpg',
+                src: '/img/Muhamka-galeri/kegiatan-siswa/kegiatan-siswa-slide11.jpg',
             },
             {
-                src: 'https://i.ibb.co/fXy8Mp1/kegiatan-siswa-slide12.jpg',
+                src: '/img/Muhamka-galeri/kegiatan-siswa/kegiatan-siswa-slide12.jpg',
             },
             {
-                src: 'https://i.ibb.co/3RNjrQJ/kegiatan-siswa-slide13.jpg',
+                src: '/img/Muhamka-galeri/kegiatan-siswa/kegiatan-siswa-slide13.jpg',
             },
             {
-                src: 'https://i.ibb.co/NKDQJ1N/kegiatan-siswa-slide14.jpg',
+                src: '/img/Muhamka-galeri/kegiatan-siswa/kegiatan-siswa-slide14.jpg',
             },
             {
-                src: 'https://i.ibb.co/JdRnnTS/kegiatan-siswa-slide15.jpg',
+                src: '/img/Muhamka-galeri/kegiatan-siswa/kegiatan-siswa-slide15.jpg',
             },
             {
-                src: 'https://i.ibb.co/9Tb8205/kegiatan-siswa-slide16.jpg',
+                src: '/img/Muhamka-galeri/kegiatan-siswa/kegiatan-siswa-slide16.jpg',
             },
             {
-                src: 'https://i.ibb.co/6nYjMbs/kegiatan-siswa-slide17.jpg',
+                src: '/img/Muhamka-galeri/kegiatan-siswa/kegiatan-siswa-slide17.jpg',
             },
             {
-                src: 'https://i.ibb.co/7j7fwrf/kegiatan-siswa-slide18.jpg',
+                src: '/img/Muhamka-galeri/kegiatan-siswa/kegiatan-siswa-slide18.jpg',
             },
             {
-                src: 'https://i.ibb.co/PcN5ptH/kegiatan-siswa-slide19.jpg',
+                src: '/img/Muhamka-galeri/kegiatan-siswa/kegiatan-siswa-slide19.jpg',
             },
     ],
         gallery: {
@@ -193,19 +193,19 @@ $(document).ready(function() {
     $('.fortasi-btn').magnificPopup({
         items: [
             {
-                src: 'https://i.ibb.co/zxVNqDh/fortasi-slide2.jpg',
+                src: '/img/Muhamka-galeri/fortasi/fortasi-slide1.jpg',
             },
             {
-                src: 'https://i.ibb.co/hBnbjDh/fortasi-slide1.jpg',
+                src: '/img/Muhamka-galeri/fortasi/fortasi-slide2.jpg',
             },
             {
-                src: 'https://i.ibb.co/XS60sCB/fortasi-slide3.jpg',
+                src: '/img/Muhamka-galeri/fortasi/fortasi-slide3.jpg',
             },
             {
-                src: 'https://i.ibb.co/xHQ6Bj2/fortasi-slide4.jpg',
+                src: '/img/Muhamka-galeri/fortasi/fortasi-slide4.jpg',
             },
             {
-                src: ' https://i.ibb.co/CvHhK3B/fortasi-slide5.jpg',
+                src: '/img/Muhamka-galeri/fortasi/fortasi-slide5.jpg',
             },
     ],
         gallery: {
